8a4001d39f13f72a54d447f80f2ab2def570660a
Grant <luxons1@hotmail.com>
2021-08-09 12:48:32 +1200

    updated prefix command

--- bot/bot.py
+++ bot/bot.py
@@ -5,6 +5,7 @@ import random
 import json
 
 from discord.ext import commands
+from discord.ext.commands.errors import MissingPermissions
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -12,8 +13,8 @@ handler = logging.FileHandler(filename = 'discord.log', encoding = 'utf-8', mode
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-#intents = discord.Intents.default()
-#intents.members = True
+intents = discord.Intents.default()
+intents.members = True
 
 def get_prefix(client, message):
     with open('bot/prefixes.json', 'r') as f:
@@ -21,7 +22,7 @@ def get_prefix(client, message):
 
     return prefixes[str(message.guild.id)]
 
-client = commands.Bot(command_prefix = get_prefix)
+client = commands.Bot(command_prefix = get_prefix, intents = intents)
 
 @client.event
 async def on_ready():
@@ -39,7 +40,6 @@ async def on_ready():
                 prefixes = json.load(f)
         if guild.id not in prefixes:
             prefixes[str(guild.id)] = 'zb!'
-                
             with open('bot/prefixes.json', 'w') as f:
                 json.dump(prefixes, f, indent=4)
 #-----------------------------------------------
@@ -61,7 +61,7 @@ async def on_ready():
     await client.change_presence(status=discordStatus, activity=discordGame)
 
     print(discordGame, ' ', discordStatus)
-
+#-----------------------------------------------
 @client.event
 async def on_guild_join(guild):
     with open('bot/prefixes.json', 'r') as f:
@@ -82,7 +82,18 @@ async def on_guild_remove(guild):
     with open('bot/prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
 
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(870490634870476881)
+    await channel.send(f'Welcome {member.mention}')
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(870490634870476881)
+    await channel.send(f'{member.mention} has fucked off')
+#-----------------------------------------------
 @client.command()
+@commands.has_permissions(manage_guild=True)
 async def changeprefix(ctx, prefix):
     with open('bot/prefixes.json', 'r') as f:
         prefixes = json.load(f)
@@ -94,6 +105,10 @@ async def changeprefix(ctx, prefix):
 
     await ctx.send(f'Prefix Changed to: {prefix}')
 
+@changeprefix.error
+async def changeprefix_error(ctx, error):
+    await ctx.send(f'{ctx.author.mention} you do not have permissions to change this prefix.')
+#-----------------------------------------------
 @client.command()
 async def ping(ctx):
     await ctx.send('Pong!')
