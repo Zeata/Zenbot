d22a0a740fcefe890e690baf48515fe4bb05f0ea
Zendata <luxons1@hotmail.com>
2021-09-04 20:28:14 +1200

    added some facts

--- bot/library/cogs/fun.py
+++ bot/library/cogs/fun.py
@@ -1,5 +1,7 @@
 from discord import Member
 from typing import Optional
+from aiohttp import request
+from discord import Embed
 from discord.ext.commands import Cog, BucketType
 from discord.ext.commands import BadArgument
 from discord.ext.commands import command, cooldown
@@ -29,6 +31,38 @@ class Fun(Cog):
     @command(name="slap", aliases=["hit"])
     async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "for no reason"):
         await ctx.send(f"{ctx.author.mention} slapped {member.mention} {reason}!")
+
+    @command(name="fact")
+    @cooldown(3, 5, BucketType.guild)
+    async def animal_fact(self, ctx, animal: str):
+        if (animal := animal.lower()) in ("dog", "cat", "panda", "fox", "bird", "koala"):
+            fact_url = f"https://some-random-api.ml/facts/{animal}"
+            image_url = f"https://some-random-api.ml/img/{'birb' if animal == 'bird' else animal}"
+
+            async with request("GET", image_url, headers={}) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    image_link = data["link"]
+
+                else:
+                    image_link = None
+
+            async with request("GET", fact_url, headers={}) as response:
+                if response.status == 200:
+                    data = await response.json()
+
+                    embed = Embed(title=f"{animal.title()} fact",
+                                    description=data["fact"],
+                                    colour=ctx.author.colour)
+                    if image_link is not None:
+                        embed.set_image(url=image_link)
+                    await ctx.send(embed=embed)
+
+                else:
+                    await ctx.send(f"API returned a {response.status} status.")
+
+        else:
+            await ctx.send("No facts are available for that animal.")
 
     @slap_member.error
     async def slap_member_error(self, ctx, exc):
